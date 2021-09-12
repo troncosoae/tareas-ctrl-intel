be1a79a73298320802f7d1b5eccdbbf308defa00
atroncoso2 <atroncoso2@uc.cl>
2021-09-12 20:43:38 -0300

    fuzzy pid

--- t2/FuzzyTools.py
+++ t2/FuzzyTools.py
@@ -1,30 +1,104 @@
 import numpy as np
 from scipy.integrate import quad
+import matplotlib.pyplot as plt
 from Simulation import BasicController
 
 
 class FuzzySet:
-    def __init__(self, membership_funcion):
-        self.mf = membership_funcion
+    def __init__(self, membership_function=lambda x: 0):
+        self.membership_function = membership_function
 
     def membership_value(self, x):
-        return self.mf(x)
+        return self.membership_function(x)
 
 
-class FuzzyController(BasicController):
-    def __init__(self, input_keys=[]):
+class FuzzyPIController(BasicController):
+    def __init__(self, Ts, input_sets, output_sets):
         BasicController.__init__(self)
-        self.sets = {input_key: [] for input_key in input_keys}
-
-    def add_set(self, input_key, fuzzy_set):
-        self.sets[input_key].append(fuzzy_set)
+        self.Ts = Ts
+        self.last_error = 0
+        self.int_error = 0
+        self.input_sets = input_sets
+        self.output_sets = output_sets
 
     def fuzzify_inputs(self, inputs):
-        if inputs.keys() != self.sets.keys():
+        if inputs.keys() != self.input_sets.keys():
             raise Exception('inputs keys must be the same!')
+        membership_values = {}
+        for input_key in inputs:
+            input_value = inputs[input_key]
+            inputs_sets = self.input_sets[input_key]
+            membership_values_for_key = {}
+            for set_key in inputs_sets:
+                membership_values_for_key[set_key] = \
+                    inputs_sets[set_key].membership_value(input_value)
+            membership_values[input_key] = membership_values_for_key
+        print(membership_values)
+        return membership_values
+
+    def inference_model(self, membership_values):
+        e_membership_values = membership_values['e']
+        de_membership_values = membership_values['de']
+
+        output_functions = []
+        for e_set_key in e_membership_values:
+            for de_set_key in de_membership_values:
+                output_set = self.output_sets[
+                    f'e: {e_set_key}; de: {de_set_key}']
+                # print(output_set.membership_function(12.5), e_membership_values[e_set_key], de_membership_values[de_set_key])
+                # print(np.min([
+                #     output_set.membership_function(12.5),
+                #     e_membership_values[e_set_key],
+                #     de_membership_values[de_set_key]
+                # ]))
+
+                def get_output_function(e_set_key, de_set_key, membership_function):
+                    def output_function(x):
+                        return np.min([
+                            membership_function(x),
+                            e_membership_values[e_set_key],
+                            de_membership_values[de_set_key]
+                        ])
+                    return output_function
+                output_function = get_output_function(e_set_key, de_set_key, output_set.membership_function)
+
+                # print('l1', output_function(12.5))
+
+                output_functions.append(output_function)
+
+        print([f(12.5) for f in output_functions])
+        return output_functions
+
+    def defuzzification(self, output_functions):
+        def output_function(x):
+            return np.max([f(x) for f in output_functions])
+        # test = 12.5
+        # print([f(test) for f in output_functions])
+        # print(f'output_function({test})', output_function(test))
+        # x_list = np.linspace(-20, 20, num=2000)
+        # y_list = [output_function(x) for x in x_list]
+        # plt.plot(x_list, y_list)
+        # plt.show()
+        int_fx = quad(lambda x: output_function(x)*x, -20, 20)[0]
+        int_f = quad(lambda x: output_function(x), -20, 20)[0]
+        if int_f == 0:
+            int_f = 1e-10
+        out = int_fx/int_f
+        return out
 
     def next_u(self, error):
-        u = 0
+        e = error
+        de = (error - self.last_error)/self.Ts
+        ie = self.int_error + error
+        self.int_error = ie
+        self.last_error = error
+
+        membership_values = self.fuzzify_inputs({'e': e, 'de': de})
+        output_functions = self.inference_model(membership_values)
+        u = self.defuzzification(output_functions)
+
+        print(f'e: {e:5.5}, de: {de:5.5} -> u: {u:5.5}')
+
         return u
 
 
@@ -54,26 +128,28 @@ def trapezoid_function_generator(a, b, c, d):
 
 
 if __name__ == '__main__':
-    ramp = ramp_function_generator(1, 2)
-    print(ramp(0.5))
-    print(ramp(1.5))
-    print(ramp(2.5))
-    ramp = ramp_function_generator(2, 1)
-    print(ramp(0.5))
-    print(ramp(1.5))
-    print(ramp(2.5))
-    print('-----')
-    trapezoid = trapezoid_function_generator(1, 2, 4, 5)
-    print(trapezoid(0.5))
-    print(trapezoid(1.5))
-    print(trapezoid(3))
-    print(trapezoid(4.5))
-    print(trapezoid(5.5))
-    print(quad(trapezoid, 0, 10))
-    trapezoid = trapezoid_function_generator(1.5, 1.51, 4.5, 4.51)
-    print(trapezoid(0.5))
-    print(trapezoid(1.5))
-    print(trapezoid(3))
-    print(trapezoid(4.5))
-    print(trapezoid(5.5))
-    print(quad(trapezoid, 0, 10))
+    # ramp = ramp_function_generator(1, 2)
+    # print(ramp(0.5))
+    # print(ramp(1.5))
+    # print(ramp(2.5))
+    # ramp = ramp_function_generator(2, 1)
+    # print(ramp(0.5))
+    # print(ramp(1.5))
+    # print(ramp(2.5))
+    # print('-----')
+    # trapezoid = trapezoid_function_generator(1, 2, 4, 5)
+    # print(trapezoid(0.5))
+    # print(trapezoid(1.5))
+    # print(trapezoid(3))
+    # print(trapezoid(4.5))
+    # print(trapezoid(5.5))
+    # print(quad(trapezoid, 0, 10))
+    # trapezoid = trapezoid_function_generator(1.5, 1.51, 4.5, 4.51)
+    # print(trapezoid(0.5))
+    # print(trapezoid(1.5))
+    # print(trapezoid(3))
+    # print(trapezoid(4.5))
+    # print(trapezoid(5.5))
+    # print(quad(trapezoid, 0, 10))
+    fs = FuzzySet()
+    print(fs.membership_value(0))
--- t2/main.py
+++ t2/main.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from Simulation import Simulation
 from AutomaticControllers import PIDController, ExpertController, \
     ExpertPIController
-from FuzzyTools import FuzzyController
+from FuzzyTools import FuzzyPIController, FuzzySet, \
+    trapezoid_function_generator, ramp_function_generator
 from Measurers import PlotingMeasurer
 
 
@@ -21,7 +22,39 @@ if __name__ == '__main__':
 
     # controller = PIDController(-74, -110, -12, Ts)
     # controller = ExpertPIController(Ts)
-    controller = FuzzyController(['e', 'de'])
+    input_sets = {
+        'e': {
+            'pos': FuzzySet(ramp_function_generator(0.15, 0.16)),
+            'zer': FuzzySet(trapezoid_function_generator(-0.15, -0.14, 0.14, 0.15)),
+            'neg': FuzzySet(ramp_function_generator(-0.15, -0.16))
+        },
+        'de': {
+            'pos': FuzzySet(ramp_function_generator(1, 1.1)),
+            'zer': FuzzySet(trapezoid_function_generator(-1, -0.9, 0.9, 1)),
+            'neg': FuzzySet(ramp_function_generator(-1, -1.1))
+        }
+    }
+    output_sets = {
+        # 'e: pos; de: pos': FuzzySet(trapezoid_function_generator(-15.5, -15, -15, -14.5)),
+        # 'e: pos; de: zer': FuzzySet(trapezoid_function_generator(-13, -12.5, -12.5, -12)),
+        # 'e: pos; de: neg': FuzzySet(trapezoid_function_generator(-10.5, -10, -10, -9.5)),
+        # 'e: zer; de: pos': FuzzySet(trapezoid_function_generator(-3, -2.5, -2.5, -2)),
+        # 'e: zer; de: zer': FuzzySet(trapezoid_function_generator(-0.5, 0, 0, 0.5)),
+        # 'e: zer; de: neg': FuzzySet(trapezoid_function_generator(2, 2.5, 2.5, 3)),
+        # 'e: neg; de: pos': FuzzySet(trapezoid_function_generator(9.5, 10, 10, 10.5)),
+        # 'e: neg; de: zer': FuzzySet(trapezoid_function_generator(12, 12.5, 12.5, 13)),
+        # 'e: neg; de: neg': FuzzySet(trapezoid_function_generator(14.5, 15, 15, 15.5))
+        'e: pos; de: pos': FuzzySet(trapezoid_function_generator(-20, -15, -15, -10)),
+        'e: pos; de: zer': FuzzySet(trapezoid_function_generator(-15, -12.5, -12.5, -10)),
+        'e: pos; de: neg': FuzzySet(trapezoid_function_generator(-15, -10, -10, -5)),
+        'e: zer; de: pos': FuzzySet(trapezoid_function_generator(-5, -2.5, -2.5, 0)),
+        'e: zer; de: zer': FuzzySet(trapezoid_function_generator(-5, 0, 0, 5)),
+        'e: zer; de: neg': FuzzySet(trapezoid_function_generator(0, 2.5, 2.5, 5)),
+        'e: neg; de: pos': FuzzySet(trapezoid_function_generator(5, 10, 10, 15)),
+        'e: neg; de: zer': FuzzySet(trapezoid_function_generator(10, 12.5, 12.5, 15)),
+        'e: neg; de: neg': FuzzySet(trapezoid_function_generator(10, 15, 15, 20))
+    }
+    controller = FuzzyPIController(Ts, input_sets, output_sets)
     sim.add_controller(controller)
     measurer = PlotingMeasurer()
     sim.add_measurer(measurer)
