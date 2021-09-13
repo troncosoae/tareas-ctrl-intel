a2acd53db652a1e2f8f2b8e9b86be77da15e6157
atroncoso2 <atroncoso2@uc.cl>
2021-09-12 23:41:35 -0300

    fuzzy expert ready to test for stability

--- t2/FuzzyTools.py
+++ t2/FuzzyTools.py
@@ -12,6 +12,80 @@ class FuzzySet:
         return self.membership_function(x)
 
 
+class FuzzyExpertController(BasicController):
+    def __init__(self, Ts, input_sets, output_sets):
+        BasicController.__init__(self)
+        self.Ts = Ts
+        self.last_error = 0
+        self.int_error = 0
+        self.input_sets = input_sets
+        self.output_sets = output_sets
+
+    def fuzzify_inputs(self, inputs):
+        if inputs.keys() != self.input_sets.keys():
+            raise Exception('inputs keys must be the same!')
+        membership_values = {}
+        for input_key in inputs:
+            input_value = inputs[input_key]
+            inputs_sets = self.input_sets[input_key]
+            membership_values_for_key = {}
+            for set_key in inputs_sets:
+                membership_values_for_key[set_key] = \
+                    inputs_sets[set_key].membership_value(input_value)
+            membership_values[input_key] = membership_values_for_key
+        print(membership_values)
+        return membership_values
+
+    def inference_model(self, membership_values):
+        e_membership_values = membership_values['e']
+
+        output_functions = []
+        for e_set_key in e_membership_values:
+            output_set = self.output_sets[
+                f'e: {e_set_key}']
+
+            def get_output_function(
+                    e_set_key, membership_function):
+                def output_function(x):
+                    return np.min([
+                        membership_function(x),
+                        e_membership_values[e_set_key]
+                    ])
+                return output_function
+            output_function = get_output_function(
+                e_set_key, output_set.membership_function)
+
+            output_functions.append(output_function)
+
+        print([f(12.5) for f in output_functions])
+        return output_functions
+
+    def defuzzification(self, output_functions):
+        def output_function(x):
+            return np.max([f(x) for f in output_functions])
+        int_fx = quad(lambda x: output_function(x)*x, -20, 20)[0]
+        int_f = quad(lambda x: output_function(x), -20, 20)[0]
+        if int_f == 0:
+            int_f = 1e-10
+        out = int_fx/int_f
+        return out
+
+    def next_u(self, error):
+        e = error
+        de = (error - self.last_error)/self.Ts
+        ie = self.int_error + error
+        self.int_error = ie
+        self.last_error = error
+
+        membership_values = self.fuzzify_inputs({'e': e})
+        output_functions = self.inference_model(membership_values)
+        u = self.defuzzification(output_functions)
+
+        print(f'e: {e:5.5}, de: {de:5.5} -> u: {u:5.5}')
+
+        return u
+
+
 class FuzzyPIController(BasicController):
     def __init__(self, Ts, input_sets, output_sets):
         BasicController.__init__(self)
--- t2/main.py
+++ t2/main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from Simulation import Simulation
 from AutomaticControllers import PIDController, ExpertController, \
     ExpertPIController
-from FuzzyTools import FuzzyPIController, FuzzySet, \
+from FuzzyTools import FuzzyPIController, FuzzyExpertController, FuzzySet, \
     trapezoid_function_generator, ramp_function_generator
 from Measurers import PlotingMeasurer
 
@@ -24,28 +24,29 @@ if __name__ == '__main__':
     # controller = ExpertPIController(Ts)
     input_sets = {
         'e': {
-            'pos': FuzzySet(ramp_function_generator(0.12, 0.18)),
-            'zer': FuzzySet(trapezoid_function_generator(-0.18, -0.12, 0.12, 0.18)),
-            'neg': FuzzySet(ramp_function_generator(-0.12, -0.18))
-        },
-        'de': {
-            'pos': FuzzySet(ramp_function_generator(0.7, 1.3)),
-            'zer': FuzzySet(trapezoid_function_generator(-1.3, -0.7, 0.7, 1.3)),
-            'neg': FuzzySet(ramp_function_generator(-0.7, -1.3))
+            'bpos': FuzzySet(ramp_function_generator(0.35, 0.36)),
+            'mpos': FuzzySet(trapezoid_function_generator(0.25, 0.26, 0.34, 0.35)),
+            'spos': FuzzySet(trapezoid_function_generator(0.15, 0.16, 0.24, 0.25)),
+            'sspos': FuzzySet(trapezoid_function_generator(0.05, 0.06, 0.14, 0.15)),
+            'zer': FuzzySet(trapezoid_function_generator(-0.05, -0.04, 0.04, 0.05)),
+            'ssneg': FuzzySet(trapezoid_function_generator(-0.15, -0.14, -0.06, -0.05)),
+            'sneg': FuzzySet(trapezoid_function_generator(-0.25, -0.24, -0.16, -0.15)),
+            'mneg': FuzzySet(trapezoid_function_generator(-0.35, -0.34, -0.26, -0.25)),
+            'bneg': FuzzySet(ramp_function_generator(-0.35, -0.36))
         }
     }
     output_sets = {
-        'e: pos; de: pos': FuzzySet(trapezoid_function_generator(-17, -15, -15, -13)),
-        'e: pos; de: zer': FuzzySet(trapezoid_function_generator(-14.5, -12.5, -12.5, -10.5)),
-        'e: pos; de: neg': FuzzySet(trapezoid_function_generator(-11, -10, -10, -9)),
-        'e: zer; de: pos': FuzzySet(trapezoid_function_generator(-4, -2.5, -2.5, -1)),
-        'e: zer; de: zer': FuzzySet(trapezoid_function_generator(-1.5, 0, 0, 1.5)),
-        'e: zer; de: neg': FuzzySet(trapezoid_function_generator(1, 2.5, 2.5, 4)),
-        'e: neg; de: pos': FuzzySet(trapezoid_function_generator(9, 10, 10, 11)),
-        'e: neg; de: zer': FuzzySet(trapezoid_function_generator(10.5, 12.5, 12.5, 14.5)),
-        'e: neg; de: neg': FuzzySet(trapezoid_function_generator(13, 15, 15, 17))
+        'e: bpos': FuzzySet(trapezoid_function_generator(-15.5, -15, -15, -14.5)),
+        'e: mpos': FuzzySet(trapezoid_function_generator(-8.8, -7.8, -7.8, -6.8)),
+        'e: spos': FuzzySet(trapezoid_function_generator(-2.5, -1.5, -1.5, -0.5)),
+        'e: sspos': FuzzySet(trapezoid_function_generator(-0.000, -0.111, -0.111, -0.222)),
+        'e: zer': FuzzySet(trapezoid_function_generator(-1.5, 0, 0, 1.5)),
+        'e: ssneg': FuzzySet(trapezoid_function_generator(0.222, 0.111, 0.111, 0.000)),
+        'e: sneg': FuzzySet(trapezoid_function_generator(0.5, 1.5, 1.5, 2.5)),
+        'e: mneg': FuzzySet(trapezoid_function_generator(6.8, 7.8, 7.8, 8.8)),
+        'e: bneg': FuzzySet(trapezoid_function_generator(14.5, 15, 15, 15.5))
     }
-    controller = FuzzyPIController(Ts, input_sets, output_sets)
+    controller = FuzzyExpertController(Ts, input_sets, output_sets)
     sim.add_controller(controller)
     measurer = PlotingMeasurer()
     sim.add_measurer(measurer)
