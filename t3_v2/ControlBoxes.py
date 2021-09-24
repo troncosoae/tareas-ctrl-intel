c39b3d7661e6e9eef9fff9dae7d59f56edd1b1e9
atroncoso2 <atroncoso2@uc.cl>
2021-09-24 14:09:52 -0300

    lqr submodels

--- t3_v2/ControlBoxes.py
+++ t3_v2/ControlBoxes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from Simulation import SimulationBox
 
 
@@ -45,5 +47,38 @@ class LQRController(SimulationBox):
         u = 0
         for k in self.inputs_keys:
             u += self.K[k]*input_values[k]
+
+        return {self.man_v_name: u}
+
+
+class LQRSubmodelController(SimulationBox):
+
+    def __init__(self, key, inputs_keys, man_v_name, K1, K2, K3):
+        SimulationBox.__init__(
+            self, key, inputs_keys, [man_v_name])
+        self.man_v_name = man_v_name
+
+        self.K1 = K1
+        self.K2 = K2
+        self.K3 = K3
+
+    def advance(self, input_values):
+        super().advance(input_values)
+
+        K = self.K1
+        if (np.abs(input_values['theta']) > np.pi/8 and
+                np.abs(input_values['theta_dot']) < 1.5):
+            print('K2')
+            K = self.K2
+        elif (np.abs(input_values['theta']) <= np.pi/8 and
+                np.abs(input_values['theta_dot']) >= 1.5):
+            K = self.K3
+            print('K3')
+        else:
+            print('K1')
+
+        u = 0
+        for k in self.inputs_keys:
+            u += K[k]*input_values[k]
 
         return {self.man_v_name: u}
--- t3_v2/ModelBoxes.py
+++ t3_v2/ModelBoxes.py
@@ -100,13 +100,13 @@ class LinearPendulumModel(SimulationBox):
 
         self.state['theta'] += c['Ts']*theta_dot
         self.state['theta_dot'] += c['Ts']*(
-            -(c['M'] + c['m'])*c['g']/(c['M']*c['l'])*theta +
-            1/(c['M']*c['l'])*F
+            -c['m']*c['g']/c['M']*theta +
+            1/c['M']*F
         )
         self.state['x'] += c['Ts']*x_dot
         self.state['x_dot'] += c['Ts']*(
-            -c['m']*c['g']/c['M']*theta +
-            1/c['M']*F
+            -(c['M'] + c['m'])*c['g']/(c['M']*c['l'])*theta +
+            1/(c['M']*c['l'])*F
         )
         self.state['F'] += c['Ts']*(-100*F + 100*u)
 
@@ -121,3 +121,113 @@ class LinearPendulumModel(SimulationBox):
         # print(preds)
 
         return preds
+
+
+class PendulumSubModelsModel(SimulationBox):
+    def __init__(self, key, Ts, **kwargs):
+        SimulationBox.__init__(
+            self, key, ['u', 'theta', 'theta_dot', 'x', 'x_dot', 'F'],
+            ['theta_pred', 'theta_dot_pred', 'x_pred', 'x_dot_pred', 'F_pred'])
+        self.cts = {
+            'Ts': Ts, 'fs': 1/Ts, 'l': 0.67, 'M': 1, 'm': 0.34, 'g': 9.8}
+
+        self.state = {
+            'F': 0,
+            'theta': kwargs.get('theta_0', 0),
+            'theta_dot': kwargs.get('theta_dot_0', 0),
+            'x': kwargs.get('x_0', 0),
+            'x_dot': kwargs.get('x_dot_0', 0),
+        }
+
+    def linear_model1(self, input_values):
+        u = input_values['u']
+        c = self.cts
+        theta = self.state['theta']
+        theta_dot = self.state['theta_dot']
+        x = self.state['x']
+        x_dot = self.state['x_dot']
+        F = self.state['F']
+
+        self.state['theta'] += c['Ts']*theta_dot
+        self.state['theta_dot'] += c['Ts']*(
+            -c['m']*c['g']/c['M']*theta +
+            1/c['M']*F
+        )
+        self.state['x'] += c['Ts']*x_dot
+        self.state['x_dot'] += c['Ts']*(
+            -(c['M'] + c['m'])*c['g']/(c['M']*c['l'])*theta +
+            1/(c['M']*c['l'])*F
+        )
+        self.state['F'] += c['Ts']*(-100*F + 100*u)
+
+        preds = {
+            'theta_pred': self.state['theta'],
+            'theta_dot_pred': self.state['theta_dot'],
+            'x_pred': self.state['x'],
+            'x_dot_pred': self.state['x_dot'],
+            'F_pred': self.state['F']
+        }
+
+        return preds
+
+    def linear_model2(self, input_values):
+
+        theta_0 = np.pi/4
+        theta_dot_0 = 0
+
+        u = input_values['u']
+        c = self.cts
+        theta = self.state['theta']
+        theta_dot = self.state['theta_dot']
+        x = self.state['x']
+        x_dot = self.state['x_dot']
+        F = self.state['F']
+
+        m = c['m']
+        M = c['M']
+        l = c['l']
+        g = c['g']
+
+        self.state['theta'] += c['Ts']*theta_dot
+        self.state['theta_dot'] += c['Ts']*(
+            ((M+m)*c['g']*np.sin(theta_0) +
+             m*l*np.cos(theta_0)*np.sin(theta_0)*theta_dot_0**2
+             )*(-2*m*l*np.cos(theta_0)*np.sin(theta_0))
+            /
+            (m*l*np.cos(theta_0)**2 - (M + m)*l
+             )*theta +
+            (2*m*l*np.cos(theta_0)*np.sin(theta_0)*theta_dot_0)
+            /
+            (m*l*np.cos(theta_0)**2 - (M + m)*l
+             )*theta_dot +
+            1/(M*l)*F
+        )
+        self.state['x'] += c['Ts']*x_dot
+        self.state['x_dot'] += c['Ts']*(
+            (m*np.sin(theta_0)*(l*theta_dot_0**2-g*np.cos(theta_0)))/(
+             M+m-m*l*np.cos(theta_0)**2)*theta +
+            (2*m*l*np.sin(theta_0)*theta_dot_0)/(
+             M+m-m*l*np.cos(theta_0))*theta_dot +
+            1/(M+m+m*l*np.cos(theta_0)**2)*F
+        )
+        self.state['F'] += c['Ts']*(-100*F + 100*u)
+
+        preds = {
+            'theta_pred': self.state['theta'],
+            'theta_dot_pred': self.state['theta_dot'],
+            'x_pred': self.state['x'],
+            'x_dot_pred': self.state['x_dot'],
+            'F_pred': self.state['F']
+        }
+
+        return preds
+
+    def advance(self, input_values):
+        super().advance(input_values)
+
+        if np.abs(input_values['theta']) > np.pi/8:
+            print('linear model 2')
+            return self.linear_model2(input_values)
+        else:
+            print('linear model 1')
+            return self.linear_model1(input_values)
