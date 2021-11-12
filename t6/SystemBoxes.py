6dd9e08c42834b312dcdc4da42c25e32ce87ca7d
atroncoso2 <atroncoso2@uc.cl>
2021-11-12 17:24:14 -0300

    progress missing one box and controler

--- t6/SystemBoxes.py
+++ t6/SystemBoxes.py
@@ -3,6 +3,27 @@ import numpy as np
 from Simulation import SimulationBox
 
 
+class InputBox(SimulationBox):
+    def __init__(self, key, Ts, output_keys, **kwargs):
+        SimulationBox.__init__(
+            self, key, [], output_keys)
+        self.output_keys = output_keys
+        self.counter = 0
+        self.on = True
+
+    def advance(self, input_values):
+        super().advance(input_values)
+        self.counter += 1
+        if self.counter > 1000:
+            self.counter = 0
+            self.on = not self.on
+        print(self.on)
+        return_dict = {}
+        for key in self.output_keys:
+            return_dict[key] = 1 if self.on else 0
+        return return_dict
+
+
 class WindModel(SimulationBox):
     def __init__(self, key, Ts, **kwargs):
         SimulationBox.__init__(
@@ -40,6 +61,112 @@ class WindModel(SimulationBox):
             'v_ws': self.state['v_ws'],
             'v_ts': self.state['v_ts'],
             'v_W': v_W,
+        }
+
+
+class DriveTrainModel(SimulationBox):
+    def __init__(self, key, Ts, **kwargs):
+        SimulationBox.__init__(
+            self, key, ['tau_r', 'tau_g'], ['omega_g', 'omega_r', 'omega_rm'])
+
+        self.chars = {
+            'Ts': Ts,
+            'Bdt': kwargs.get('Bdt', 775.49),
+            'Br': kwargs.get('Br', 7.11),
+            'Bg': kwargs.get('Bg', 45.6),
+            'Ng': kwargs.get('Ng', 95),
+            'Kdt': kwargs.get('Kdt', 2.7e9),
+            'nu_dt': kwargs.get('nu_dt', 0.97),
+            'Jg': kwargs.get('Jg', 390),
+            'Jr': kwargs.get('Jr', 55e6),
+        }
+        self.state = {
+            'omega_r': kwargs.get('omega_r_0', 0),
+            'omega_g': kwargs.get('omega_g_0', 0),
+            'theta_d': kwargs.get('theta_d_0', 0),
+        }
+
+    def advance(self, input_values):
+        super().advance(input_values)
+
+        tau_r = input_values['tau_r']
+        tau_g = input_values['tau_g']
+        wr = self.state['omega_r']
+        wg = self.state['omega_g']
+        td = self.state['theta_d']
+        Ts = self.chars['Ts']
+        Bdt = self.chars['Bdt']
+        Br = self.chars['Br']
+        Bg = self.chars['Bg']
+        Ng = self.chars['Ng']
+        Kdt = self.chars['Kdt']
+        nu_dt = self.chars['nu_dt']
+        Jg = self.chars['Jg']
+        Jr = self.chars['Jr']
+
+        self.state['omega_r'] += Ts/Jr*(
+            tau_r - Kdt*td - (Bdt + Br)*wr + Bdt/Ng*wg
+        )
+        self.state['omega_g'] += Ts/Jg*(
+            nu_dt*Kdt/Ng*td + nu_dt*Bdt/Ng*wr -
+            (nu_dt*Bdt/Ng**2 + Bg)*wg - tau_g
+        )
+        self.state['theta_d'] += Ts*(
+            wr - 1/Ng*wg
+        )
+
+        return {
+            'omega_g': self.state['omega_g'],
+            'omega_r': self.state['omega_r'],
+            'omega_rm': self.state['omega_r'],
+        }
+
+
+class BladePitchSystem(SimulationBox):
+    def __init__(self, key, Ts, **kwargs):
+        SimulationBox.__init__(
+            self, key, ['v_W', 'beta_r', 'omega_r'], ['beta_m', 'tau_r'])
+
+        def default_Cq(lmbda, beta):
+            return 0.45*np.cos(beta)/lmbda
+
+        self.chars = {
+            'zeta': kwargs.get('zeta', 0.6),  # damping factor
+            'omega': kwargs.get('omega', 11.11),  # natural freq
+            'ro': kwargs.get('ro', 1.225),  # ro
+            'R': kwargs.get('R', 57.5),  # R (m)
+            'Cq': kwargs.get('Cq', default_Cq),  # Cq(lambda, beta)
+            'Ts': Ts,
+        }
+        self.state = {
+            'beta_m': kwargs.get('beta_m_0', 0),
+            'beta_m_dot': kwargs.get('beta_m_dot_0', 0),
+        }
+
+    def advance(self, input_values):
+        super().advance(input_values)
+        br = input_values['beta_r']
+        v_W = input_values['v_W']
+        bm = self.state['beta_m']
+        bm_dot = self.state['beta_m_dot']
+        Ts = self.chars['Ts']
+        z = self.chars['zeta']
+        w = self.chars['omega']
+        ro = self.chars['ro']
+        R = self.chars['R']
+        Cq = self.chars['Cq']
+
+        self.state['beta_m'] += Ts*bm_dot
+        self.state['beta_m_dot'] += Ts*(
+            br*w**2 - 2*z*w*bm_dot - w**2*bm
+        )
+
+        lmbda = w*R/v_W
+        tau_r = (ro*np.pi*R**3*Cq(lmbda, bm)*v_W**2)/2
+
+        return {
+            'tau_r': tau_r,
+            'beta_m': self.state['beta_m'],
         }
 
 
--- t6/main.py
+++ t6/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from Simulation import Simulation, get_close_sim_for_box
-from SystemBoxes import PendulumSystem, WindModel
+from SystemBoxes import PendulumSystem, WindModel, BladePitchSystem, \
+    InputBox, DriveTrainModel
 from PygameBoxes import PendulumWindow, TurbineWindow
 from ControlBoxes import PIDController, LQRController, LQRSubmodelController
 from MeasuringBoxes import PlottingMeasurer
@@ -19,19 +20,28 @@ if __name__ == "__main__":
     sim = Simulation()
 
     wind_model = WindModel('wind_model', Ts)
+    blade_pitch_system = BladePitchSystem('bp_sys', Ts)
+    drive_train_model = DriveTrainModel('dt_model', Ts)
     measurer = PlottingMeasurer(
         'meas',
-        ['v_m', 'v_s', 'v_ws', 'v_ts', 'v_W'],
+        [
+            'v_m', 'v_s', 'v_ws', 'v_ts', 'v_W', 'beta_m', 'beta_r', 'omega_r',
+            'omega_g'
+        ],
         Ts)
     pygame_tracker = TurbineWindow(
         'pygame', [], pygame_fs, get_close_sim_for_box(sim))
+    input_box = InputBox('inpt_box', Ts, ['beta_r'])
 
     sim.add_box(wind_model)
+    sim.add_box(blade_pitch_system, {'beta_r': np.pi/8, 'omega_r': 0})
+    sim.add_box(drive_train_model, {'tau_g': 0})
     sim.add_box(measurer)
     sim.add_box(pygame_tracker)
+    sim.add_box(input_box)
 
     sim.run()
 
     pygame_tracker.quit_pygame()
 
-    measurer.plot_values(['v_m', 'v_s', 'v_ws', 'v_ts', 'v_W'])
+    measurer.plot_values()
